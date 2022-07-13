cdca359e338cf915f7c58c372f23f01096b22033
TSKI433 <64669281+TSKI433@users.noreply.github.com>
2022-07-13 18:43:15 +0800

    对控制面板的主题管理进行模块拆分

--- src/preload/control/index.js
+++ src/preload/control/index.js
@@ -1,8 +1,9 @@
-import { contextBridge, ipcRenderer } from "electron";
+import { contextBridge } from "electron";
 import { shell } from "electron";
 import * as database from "./database";
 import * as config from "./config";
 import * as ipc from "./ipc";
+import "./theme";
 contextBridge.exposeInMainWorld("nodeAPI", {
   database,
   config,
@@ -11,28 +12,4 @@ contextBridge.exposeInMainWorld("nodeAPI", {
 });
 function showInFolder(path) {
   shell.showItemInFolder(path);
-}
-// document.addEventListener("DOMContentLoaded", () => {
-//   ipcRenderer.invoke("control:query-system-theme").then((theme) => {
-// updateTheme(theme);
-//   });
-// });
-// 经测试，采用异步函数加载深色主题时，会出现又浅色到深色主题的闪烁，因此只能改用同步阻塞函数
-// 放到DOMContentLoaded之前有助于减少等待时长，虽然说相对于DOMContentLoaded完成所花的几百甚至一千多ms来讲，这里省下的约0.5ms可能什么都不是
-// console.time("control:query-system-theme");
-const theme = ipcRenderer.sendSync("control:query-system-theme");
-// 不放在DOMContentLoaded事件之后会导致document.documentElement为null
-document.addEventListener("DOMContentLoaded", () => {
-  // console.timeEnd("control:query-system-theme");
-  updateTheme(theme);
-});
-ipcRenderer.on("main:update-theme", (evnet, theme) => {
-  updateTheme(theme);
-});
-function updateTheme(theme) {
-  if (theme === "dark") {
-    document.documentElement.className = "dark";
-  } else if (theme === "light") {
-    document.documentElement.className = "";
-  }
 }
---
+++ src/preload/control/theme.js
@@ -0,0 +1,25 @@
+import { ipcRenderer } from "electron";
+// document.addEventListener("DOMContentLoaded", () => {
+//   ipcRenderer.invoke("control:query-system-theme").then((theme) => {
+// updateTheme(theme);
+//   });
+// });
+// 经测试，采用异步函数加载深色主题时，会出现又浅色到深色主题的闪烁，因此只能改用同步阻塞函数
+// 放到DOMContentLoaded之前有助于减少等待时长，虽然说相对于DOMContentLoaded完成所花的几百甚至一千多ms来讲，这里省下的约0.5ms可能什么都不是
+// console.time("control:query-system-theme");
+const theme = ipcRenderer.sendSync("control:query-system-theme");
+// 不放在DOMContentLoaded事件之后会导致document.documentElement为null
+document.addEventListener("DOMContentLoaded", () => {
+  // console.timeEnd("control:query-system-theme");
+  updateTheme(theme);
+});
+ipcRenderer.on("main:update-theme", (evnet, theme) => {
+  updateTheme(theme);
+});
+function updateTheme(theme) {
+  if (theme === "dark") {
+    document.documentElement.className = "dark";
+  } else if (theme === "light") {
+    document.documentElement.className = "";
+  }
+}
