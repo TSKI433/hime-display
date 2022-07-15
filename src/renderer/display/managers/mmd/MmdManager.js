6b7ef23bc9c6010ac964d6085dad769cfbafb802
TSKI433 <64669281+TSKI433@users.noreply.github.com>
2022-07-15 08:40:20 +0800

    做了各种测试，依旧没有找到MMD载入问题的解决方案

--- src/renderer/display/managers/mmd/MmdManager.js
+++ src/renderer/display/managers/mmd/MmdManager.js
@@ -52,6 +52,21 @@ export class MmdManager {
       // ERROR:command_buffer_proxy_impl.cc(293)] GPU state invalid after WaitForTokenInRange.
       // ERROR:gpu_process_host.cc(974)] GPU process exited unexpectedly: exit_code=512
       // 经过各种瞎尝试，设个延时（实测延时设短了也不行，如0，100也有几率卡死）函数后，就不会有问题了，别问我解决的原理是什么，问就是不知道
+
+      // 这个问题在脱离Electron，直接在Chrome，Firefox中测试时都不会出现
+      // 通过控制变量法实验得出结论，该问题与ESM模块系统无直接关联，全换成iife模块它也崩
+      // 通过第二个控制变量实验得出结论，该问题与跨域使用file:// 加载模型无直接关联
+      // 顺带一提，展示器在崩坏重载后的报错信息为：
+      // THREE.WebGLProgram: Shader Error 0 - VALIDATE_STATUS false
+      // WebGL: INVALID_OPERATION: useProgram: program not valid
+      // THREE.WebGLProgram: Shader Error 1282 - VALIDATE_STATUS false
+      // INVALID_OPERATION: useProgram: program not valid
+      // INVALID_OPERATION: useProgram: program not valid
+      // INVALID_OPERATION: useProgram: program not valid
+      // WebGL: CONTEXT_LOST_WEBGL: loseContext: context lost
+      // THREE.WebGLRenderer: Context Lost.
+      // 综上，推测此问题可能比较单纯，就是在Electron环境下THREE的渲染在一些情况下会导致GPU错误。出现的第一个报错为Shader Error，推测深层次可能跟THREE的MMDToonShader的渲染有关，反正这已经到我无法解决的领域了
+
       // 震惊！！！setImmediate居然不是标准特性……
       // https://developer.mozilla.org/zh-CN/docs/Web/API/Window/setImmediate
       // 直接给我来个ReferenceError: setImmediate is not defined
