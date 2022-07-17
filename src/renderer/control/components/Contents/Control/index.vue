b2c73008becb72deda690a04a2d39198459a5b4b
TSKI433 <64669281+TSKI433@users.noreply.github.com>
2022-07-17 21:06:50 +0800

    上个提交不小心交漏了……

--- src/renderer/control/components/Contents/Control/index.vue
+++ src/renderer/control/components/Contents/Control/index.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>Control</div>
+  <div class="content--hime">
+    <hime-title-with-divider>{{ $t("menu.control") }}</hime-title-with-divider>
+    <template v-if="controlComponentsNow !== undefined">
+      <component :is="controlComponentsNow"></component>
+    </template>
+    <template v-else> 载入一个模型以进行控制 </template>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import HimeTitleWithDivider from "@control/components/Common/TitleWithDivider.vue";
+import HimeMMDControl from "@control/components/Contents/Control/MMDControl/index.vue";
+import HimeSpineControl from "@control/components/Contents/Control/SpineControl/index.vue";
+import HimeVRoidControl from "@control/components/Contents/Control/VRoidControl/index.vue";
+import HimeLive2DControl from "@control/components/Contents/Control/Live2DControl/index.vue";
+import { useControlStore } from "@control/store/control";
+import { computed } from "vue";
+const controlStore = useControlStore();
+const controlComponents = {
+  HimeMMDControl,
+  HimeSpineControl,
+  HimeVRoidControl,
+  HimeLive2DControl,
+};
+const controlComponentsNow = computed(() => {
+  return controlComponents[`Hime${controlStore.currentModelType}Control`];
+});
+</script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.model-control--hime {
+  flex: 1;
+}
+</style>
