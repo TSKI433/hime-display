7d98b1ee322e417147e0c39c4400d05d3e86416c
TSKI433 <64669281+TSKI433@users.noreply.github.com>
2022-07-29 08:18:16 +0800

    MMD面部捕捉的眨眼暂时改为まばたき变形，wink效果的实现有待商议

--- src/renderer/display/utils/capture/MMDFaceMeshCaptureManager.js
+++ src/renderer/display/utils/capture/MMDFaceMeshCaptureManager.js
@@ -1,5 +1,5 @@
 import { FaceMeshCaptureManager } from "./FaceMeshCaptureManager.js";
-import { Vector } from "kalidokit";
+import { Vector, Face } from "kalidokit";
 const { lerp } = Vector;
 export class MMDFaceMeshCaptureManager extends FaceMeshCaptureManager {
   constructor() {
@@ -14,8 +14,34 @@ export class MMDFaceMeshCaptureManager extends FaceMeshCaptureManager {
     this._getBoneNode("左腕").rotation.z = -0.5;
   }
   rigFace(riggedFace, lerpRatio = 0.5) {
-    this.lerpMorphTargetByName("ウィンク２", 1 - riggedFace.eye.l, lerpRatio);
-    this.lerpMorphTargetByName("ウィンク２右", 1 - riggedFace.eye.r, lerpRatio);
+    // 使用Kalidokit提供的stabilizeBlink对眨眼进行进一步优化，还有得进一步考虑wink的启动机制，ウィンク与まばたき变形的关系有待考虑，目前先全部改用まばたき变形
+    const BlinkMorphIndex = this.model.morphTargetDictionary["まばたき"];
+    // debugger;
+    if (BlinkMorphIndex === undefined) {
+      console.warn(
+        `MotionCaptureManager: morph target まばたき not found in the skinned mesh`
+      );
+    } else {
+      const lerpedEye = {
+        l: lerp(
+          riggedFace.eye.l,
+          1 - this.model.morphTargetInfluences[BlinkMorphIndex],
+          lerpRatio
+        ),
+        r: lerp(
+          riggedFace.eye.r,
+          1 - this.model.morphTargetInfluences[BlinkMorphIndex],
+          lerpRatio
+        ),
+      };
+      // 我认为这里不需要用headY做一道判断，就直接将其赋值为0了
+      const stabilizedEye = Face.stabilizeBlink(lerpedEye, 0);
+      this.model.morphTargetInfluences[BlinkMorphIndex] =
+        1 - (stabilizedEye.l + stabilizedEye.r) / 2;
+    }
+
+    // this.lerpMorphTargetByName("ウィンク２", 1 - riggedFace.eye.l, lerpRatio);
+    // this.lerpMorphTargetByName("ウィンク２右", 1 - riggedFace.eye.r, lerpRatio);
     this.lerpMorphTargetByName("あ", riggedFace.mouth.shape.A, lerpRatio);
     this.lerpMorphTargetByName("い", riggedFace.mouth.shape.I, lerpRatio);
     this.lerpMorphTargetByName("う", riggedFace.mouth.shape.U, lerpRatio);
