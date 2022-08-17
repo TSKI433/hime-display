17cd0a67b81157455c9fa4f44a62aa46760c9519
TSKI433 <64669281+TSKI433@users.noreply.github.com>
2022-08-17 17:01:31 +0800

    进一步提取3D共有组件

--- src/renderer/control/components/Contents/Control/VRoidControl/index.vue
+++ src/renderer/control/components/Contents/Control/VRoidControl/index.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="hime-model-control">
+    <!-- 弃用方案：使用Menu组件构建 -->
+    <!-- <el-menu
+      @select="handleSelect"
+      mode="horizontal"
+      class="hime-el-menu--sub-control"
+      :ellipsis="false"
+      :default-active="subControlComponentNameNow"
+    >
+      <el-menu-item index="ModelDescription">模型信息</el-menu-item>
+      <el-menu-item index="ObjectTransform">对象变换</el-menu-item>
+      <el-menu-item index="AnimationControl">动画播放</el-menu-item>
+      <el-menu-item index="MotionCapture">动作捕捉</el-menu-item>
+    </el-menu> -->
     <el-tabs type="border-card">
+      <!-- 这里用v-for实在是麻烦，还是依次设置 -->
       <el-tab-pane :label="$t(`control.model-description`)">
         <model-description
           v-if="controlStore.modelControlInfo?.description !== undefined"
@@ -22,22 +36,22 @@
           <control-load-error></control-load-error>
         </template>
       </el-tab-pane>
-      <!-- <el-tab-pane :label="$t(`control.animation-control`)">
+      <el-tab-pane :label="$t(`control.animation-control`)">
         <animation-control> </animation-control>
       </el-tab-pane>
-      <el-tab-pane :label="$t(`control.morph-target`)">
-        <morph-target
+      <el-tab-pane :label="$t(`control.parameter-control`)">
+        <parameter-control
           v-if="controlStore.modelControlInfo?.morph !== undefined"
           :morph-info="controlStore.modelControlInfo.morph"
         >
-        </morph-target>
+        </parameter-control>
         <template v-else>
           <control-load-error></control-load-error>
         </template>
       </el-tab-pane>
       <el-tab-pane :label="$t(`control.motion-capture`)">
         <motion-capture> </motion-capture>
-      </el-tab-pane> -->
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -47,9 +61,9 @@ import { reactive } from "vue";
 import ControlLoadError from "../Common/ControlLoadError.vue";
 import ModelDescription from "../Common/ModelDescription.vue";
 import ObjectTransform from "../Common/3d/ObjectTransform3D.vue";
-// import AnimationControl from "./AnimationControl.vue";
-// import MorphTarget from "./MorphTarget.vue";
-// import MotionCapture from "../Common/MotionCapture.vue";
+import AnimationControl from "../Common/3d/AnimationControl.vue";
+import ParameterControl from "../Common/3d/ParameterControl.vue";
+import MotionCapture from "../Common/MotionCapture.vue";
 import { useControlStore } from "@control/store/control";
 const controlStore = useControlStore();
 const descriptionInfo = reactive({
