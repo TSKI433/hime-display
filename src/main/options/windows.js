3d5625bbbf8a4c295c13dc59200296491b68df4b
TSKI433 <TESTID135@163.com>
2025-04-26 09:27:47 +0800

    修复control的预加载无法引入lowdb

--- src/main/options/windows.js
+++ src/main/options/windows.js
@@ -20,6 +20,7 @@ export const windowsOptions = {
         // contextIsolation: false,
         // webSecurity: false,
         devTools: true,
+        sandbox: false,
         preload: resolve(__dirname, "./control-preload.cjs.js"),
       },
     },
